3e31f29c701094b36e7cbfc61ab423997f627489
oksk6685@handong.ac.kr <oksk6685@handong.ac.kr>
2023-04-30 23:10:01 +0900

    백준

---
+++ a+b.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+
+
+  using namespace std;
+
+
+  int num1, num2, sum =0;
+
+
+  
+  cin >> num1 >> num2;
+  
+  sum = num1 + num2;
+
+  cout << sum;
+
+  return 0;
+
+}
